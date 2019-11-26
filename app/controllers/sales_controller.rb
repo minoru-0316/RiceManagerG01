d05a7a0050aadb12af4f66f4721cdcbedb47f270
江渡　実 <>
2019-11-26 20:47:16 +0900

    RiceManagerG_05 デプロイ時の状態に戻した

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,6 @@
 class SalesController < ApplicationController
 
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   before_action :ensure_correct_user,{only:[:edit,:update,:destroy]}
   def ensure_correct_user
