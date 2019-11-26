5c4f0a8cabfe27f86421194bccb1edf2c0315136
江渡　実 <>
2019-11-26 21:31:35 +0900

    RiceManagerG_08 ユーザー登録を中止

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
-# class Users::RegistrationsController < Devise::RegistrationsController
-#     before_action :guard_signup!, only: %i[cancel new destroy create]
+class Users::RegistrationsController < Devise::RegistrationsController
+    before_action :guard_signup!, only: %i[cancel new destroy create]
   
-#     private
+    private
   
-#     def guard_signup!
-#       raise ActionController::RoutingError, 'NOT FOUND'
-#     end
-#   end
+    def guard_signup!
+      raise ActionController::RoutingError, 'NOT FOUND'
+    end
+  end
