a10a4a9e2bd91a87c56692ad22a849a9fb63ac5d
江渡　実 <>
2019-12-05 18:24:33 +0900

    RiceManagerG_33  実際に使ってみての修正-2

--- app/models/polish.rb
+++ app/models/polish.rb
@@ -1,5 +1,10 @@
 class Polish < ApplicationRecord
 
+    # 使用した玄米の合計
+    def self.sumpolish  
+        sum(:polish_amount_total)
+    end
+
     has_many :stock_polishes
     has_many :stocks, through: :stock_polishes
     
