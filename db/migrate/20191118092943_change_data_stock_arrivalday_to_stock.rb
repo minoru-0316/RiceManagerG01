33c184d5f2650fa321d45b845c408e69e4bba9ef
江渡　実 <>
2019-12-12 19:24:09 +0900

    fix stocks migration

--- db/migrate/20191118092943_change_data_stock_arrivalday_to_stock.rb
+++ db/migrate/20191118092943_change_data_stock_arrivalday_to_stock.rb
@@ -1,5 +1,6 @@
 class ChangeDataStockArrivaldayToStock < ActiveRecord::Migration[5.2]
   def change
-    change_column :stocks, :stock_arrivalday, :datetime
+    remove_column :stocks, :stock_arrivalday
+    add_column :stocks, :stock_arrivalday, :datetime
   end
 end
